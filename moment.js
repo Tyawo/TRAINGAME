ca46ba9df3a3308a7035fcffeb3d3f3ec669e611
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-04-13 20:27:02 -0400

    Traingame

---
+++ moment.js
@@ -0,0 +1,15 @@
+
+// variables
+//  train
+//  num, timers, departures and destination cities; input;
+var depCity = ["NEWYORK" ," MIAMI" , "CALIFORNIA" , "CHICAGO" , "BOSTON" , "HOUSTON" ];
+var destCity = []
+
+
+// Functions
+// grab user choices dep-city, dest-city, time
+
+$("#submit").on("click" , function(){
+    var depCity = 
+})
+
